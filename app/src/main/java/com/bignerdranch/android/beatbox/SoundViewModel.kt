d83e9d04162d336b9512c9de69b48f1e139204d9
AslanBolurov <aslanbolurov@list.ru>
2022-10-05 14:14:56 +0300

    initial commit
    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+
+class SoundViewModel(
+    private val beatBox: BeatBox
+):BaseObservable() {
+
+
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
+
+
+    var sound:Sound? = null
+        set(sound) {
+            field=sound
+            notifyChange()
+//            notifyPropertyChanged(BR.title)
+        }
+    @get:Bindable
+    val title:String?
+        get() = sound?.name
+
+}
